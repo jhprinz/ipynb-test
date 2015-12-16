32097143ed44144b4f1c165290cffa594e613c94
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2015-12-16 14:26:47 +0100

    add run_test

--- devtools/conda-recipe/run_test.sh
+++ devtools/conda-recipe/run_test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ipynbtest.py --eval "denom=0" --timeout 2 --restart-if-fail 1 examples/ipynbtest_tutorial.ipynb
+ipynbtest.py --eval "denom=0" --timeout 2 --restart-if-fail 1 ipynbtest_tutorial.ipynb
