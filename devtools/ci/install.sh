6c13a538d050d484c3963ef853b43cc591ce0d1d
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2016-11-04 22:42:24 +0100

    fix travis.yml

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -7,7 +7,13 @@ echo travis_fold:start:install.conda
 echo Install conda
 
 MINICONDA=Miniconda2-latest-Linux-x86_64.sh
-MINICONDA=Miniconda2-4.2.12-Linux-x86_64.sh
+
+if [[ "$CONDA_PY" == "27" ]]; then
+    MINICONDA=Miniconda2-4.2.12-Linux-x86_64.sh
+else
+    MINICONDA=Miniconda3-4.2.12-Linux-x86_64.sh
+fi
+
 MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget https://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
@@ -16,12 +22,21 @@ if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Found: $(md5sum $MINICONDA | cut -d ' ' -f 1)"
     exit 1
 fi
-bash $MINICONDA -b
 
-export PATH=$HOME/miniconda2/bin:$PATH
+bash $MINICONDA -b -p $HOME/miniconda
+
+export PATH=$HOME/miniconda/bin:$PATH
+hash -r
 
 # add omnia and update
 conda config --add channels http://conda.anaconda.org/omnia
 conda update --yes conda
 
+conda info -a
+
 echo travis_fold:end:install.conda
+
+  # Replace dep1 dep2 ... with your dependencies
+  - conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION dep1 dep2 ...
+  - source activate test-environment
+  - python setup.py install
