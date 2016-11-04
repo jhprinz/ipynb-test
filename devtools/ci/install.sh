4e9db58fd7c39e8954b8a76ce15fb25b7b91df45
Jan-Hendrik Prinz <jan.prinz@gmx.de>
2016-11-04 12:40:48 +0100

    fix travis.yml

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -1,25 +1,26 @@
+#!/bin/sh
 # This script was taken from https://github.com/pandegroup/mdtraj/tree/master/devtools
-
-sudo apt-get update
 
 ### Install Miniconda
 
-MINICONDA=Miniconda-latest-Linux-x86_64.sh
+echo travis_fold:start:install.conda
+echo Install conda
+
+MINICONDA=Miniconda2-latest-Linux-x86_64.sh
 MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget https://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
+    echo "Expected: $MINICONDA_MD5"
+    echo "Found: $(md5sum $MINICONDA | cut -d ' ' -f 1)"
     exit 1
 fi
 bash $MINICONDA -b
 
-## Install conda pacakages
-
-# This might make the --yes obsolete
-# conda config --set always_yes yes --set changeps1 no
-
 export PATH=$HOME/miniconda2/bin:$PATH
 
+# add omnia and update
 conda config --add channels http://conda.anaconda.org/omnia
 conda update --yes conda
-source activate $python
+
+echo travis_fold:end:install.conda
